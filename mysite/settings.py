1d2b176765370282c94323d2caa9bd0a5046f61b
eltonplima <eltonplima@gmail.com>
2022-02-05 12:39:04 +0000

    Configure heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 """
 Django settings for mysite project.
 
@@ -121,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
