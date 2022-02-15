6a70cc58ecf76bd44b789ed13dcc8c727feaf9b9
eltonplima <eltonplima@gmail.com>
2022-02-15 08:01:56 +0000

    Update admin

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    model = Question
+    list_display = ("question_text", "pub_date", "was_published_recently")
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
